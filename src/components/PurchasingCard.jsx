da6020548ba53259c43dc037bd8ed26053d27486
itxmujahidali <itxmujahidali@gmail.com>
2023-03-03 06:14:29 -0500

    fix styling issues in purchasingCard.jsx

--- src/components/PurchasingCard.jsx
+++ src/components/PurchasingCard.jsx
@@ -100,11 +100,11 @@ const PurchasingCard = () => {
                             <input type="text" className="form-control" id="address2" placeholder="Apartment or suite" />
                         </div>
                         <div class="form-row">
-                            <div class="form-group col-md-8">
+                            <div class="form-group col-md-8 mb-2">
                                 <label for="inputCity">City</label>
                                 <input type="text" class="form-control" id="inputCity"/>
                             </div>
-                            <div class="form-group col-md-4">
+                            <div class="form-group col-md-4 mb-2">
                                 <label for="inputState">State</label>
                                 <select id="inputState" class="form-control">
                                     <option selected>Choose...</option>
